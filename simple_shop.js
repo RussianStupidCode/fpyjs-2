b63f1d8c3110466dd30631cfe2f543d98d00666e
artur <i@aderbin.ru>
2022-03-01 18:46:07 +0500

    add homework oop

--- simple_shop.js
+++ simple_shop.js
@@ -102,4 +102,14 @@ function main(shoppingСart, goods) {
 }
 
 
-main(shoppingСart, goods);
+//main(shoppingСart, goods);
+
+const readline = require('readline').createInterface({
+    input: process.stdin,
+    output: process.stdout
+  })
+  
+  readline.question(`What's your name?`, (name) => {
+    console.log(`Hi ${name}!`)
+    readline.close()
+  })
