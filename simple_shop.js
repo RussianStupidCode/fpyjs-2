88f7c37b03a3d381683ee104452a9a410af648f0
artur <i@aderbin.ru>
2022-03-10 20:20:12 +0500

    add async

--- simple_shop.js
+++ simple_shop.js
@@ -102,14 +102,4 @@ function main(shoppingСart, goods) {
 }
 
 
-//main(shoppingСart, goods);
-
-const readline = require('readline').createInterface({
-    input: process.stdin,
-    output: process.stdout
-  })
-  
-  readline.question(`What's your name?`, (name) => {
-    console.log(`Hi ${name}!`)
-    readline.close()
-  })
+main(shoppingСart, goods);
